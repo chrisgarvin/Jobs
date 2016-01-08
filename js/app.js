dd8fe1141f726316e8f85a3e2d97ea0e03b651b0
Chris Garvin <chrisgarvin@Chriss-MacBook-2.local>
2016-01-07 18:28:16 -0800

    modified styling a bit, added a comment in js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,29 @@
 $(document).ready(function() {
 
+  //Upon clicking the submit button, run showJob
+
   $('button').click(showJob());
 
+
+  //showJob returns a function that based on the value of the dropdown
+  //and if the code was 'fixed' properly, it will redirect user to job description
+  //page, else alerts syntax error prompt
 
   function showJob() {
     return function() {
 
-      if($('input')[0].value == "</body>"){
+      if ($('input')[0].value == "</body>") {
 
-      $.getJSON("data/data.json", function(data) {
-        var job = $('select')[0].value;
-        var url = data.response.jobs[job].url;
-        console.log(data.response.jobs[job].title);
-        window.open(url);
-      });
+        $.getJSON("data/data.json", function(data) {
+          var job = $('select')[0].value;
+          var url = data.response.jobs[job].url;
+          console.log(data.response.jobs[job].title);
+          window.open(url);
+        });
+      } else {
+        alert("syntax error");
+      }
     }
-  }
   }
 
 });
