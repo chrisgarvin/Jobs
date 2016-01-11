2925f425f5f36d92f7d605162ded124578b851a6
Chris Garvin <chrisgarvin@chrissmacbook2.gateway.sonic.net>
2016-01-10 19:33:53 -0800

    added a change to the ProductDesigner dropdown, need to clean up formattig

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
 
-//updates page if user changes ID to APIProductManager
+//updates page if user changes ID to APIProductManager or ProductDesigner
 
   $('select').change(function(){
     if($('select')[0].value == 1){
+      $('.css').hide();
       $('.API').show();
-    } else {
+    } else if($('select')[0].value == 2){
       $('.API').hide();
+      $('.css').show();
+    }
+  })
+
+//checks to see if visitor clicks enter key
+
+  $("#css_submit").keyup(function(event){
+    if(event.keyCode == 13){
+
+//changes background-color of page based on user input
+
+      $('body').css('background-color', $("#css_submit")[0].value);
+
     }
   })
 
@@ -18,16 +32,19 @@ $(document).ready(function() {
 //if text box value is response.info[0].goal, greets user
 
         if($("#api_submit")[0].value == "response.info[0].goal"){
+          console.log("Greet me.");
           console.log("Well hello human, at least I assume you're human?");
 
 //if text box value is response.info[1].goal, shows user link to job description
 
         } else if($("#api_submit")[0].value == "response.info[1].goal") {
+          console.log("Show me the API Product Manager job description.");
           console.log("Hi there, we'd love to meet you! " + "https://jobs.lever.co/chain/c140624a-6dc5-4924-8742-2c1ed6e52d49");
 
 //if text box value is response.info[2].goal, 'rolls the dice'
 
         } else if($("#api_submit")[0].value == "response.info[2].goal") {
+          console.log("Roll the dice.");
           console.log(".................................................. ............................... .......,-~~'''''''~~--,,_");
           console.log(".................................................. ..................................,-~''-,:::::::::::::::::::''-,");
           console.log(".................................................. .............................,~''::::::::',::::::: :::::::::::::|',");
@@ -107,7 +124,7 @@ $(document).ready(function() {
           console.log("................................................../;;;;;;;;;;;;;;;;;;;;;;;;;;;;,;;;;;;;;;\. . .''|::::::::|. .,';;;;;;;;;;''-,");
           console.log("................................................,' ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;|;;;;;;;;;;;\. . .\:::::,'. ./|;;;;;;;;;;;;;|");
         } else {
-
+          console.log($('#api_submit')[0].value);
         }
     }
 });
